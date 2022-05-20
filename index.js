5fb7f8d57bf21b743be6cec3ff95907d2b896d6d
keganpowers34 <42286354+keganpowers34@users.noreply.github.com>
2022-05-20 17:44:46 -0600

    index.js moment

---
+++ index.js
@@ -0,0 +1,18 @@
+var weather = require('weather-js');
+var express = require('express');
+
+var app = express();
+app.get('/api/:cs&:dt', async (req, res) => {
+  const cs = req.params.cs;
+  const dt = req.params.dt;
+
+  weather.find({ search: cs, degreeType: dt }, function(err, result) {
+    if(err) console.log(err);
+
+    res.send(JSON.stringify(result));
+  });
+});
+
+app.listen(80, () => {
+  console.log("Server running on port 80");
+ });
